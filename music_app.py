805afc5e9ad413882d640bca31383de79c53ffa5
Mav Sanchez <maverickssanchez@gmail.com>
2020-05-05 21:19:40 -0700

    Updates

--- music_app.py
+++ music_app.py
@@ -1,14 +1,33 @@
+from wordcloud import WordCloud
+import imageio
+import numpy as np
 import requests
 import json
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask import abort
 from bs4 import BeautifulSoup
 
+
+from textblob import TextBlob
+from textblob import Word
+from textblob.sentiments import NaiveBayesAnalyzer
+from pathlib import Path
+import nltk
+from nltk.corpus import stopwords
+from operator import itemgetter
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 app = Flask(__name__)
 
 GENIUS_API_KEY = 'iPRpGvyPEPwHqexfQo75LsL0i2pPQxhkw-P5WStYbdvmUq-PQyf7ppCnT92Z-ZQc'
 genius_base_url = 'https://api.genius.com'
 headers = {'Authorization': 'Bearer ' + GENIUS_API_KEY}
+
+nltk.download('stopwords')
+stops = stopwords.words('english')
+
 
 class Song:
     def __init__(self, id, title, artist, url, album, year, image, lyrics ):
@@ -21,6 +40,18 @@ class Song:
         self.image = image
         self.lyrics = lyrics
 
+
+def build_wordcloud(lyrics_input):
+    song_lyrics = TextBlob(lyrics_input)
+    lyrics = song_lyrics.word_counts.items()
+    lyrics = [lyric for lyric in lyrics]
+    sorted_lyrics = sorted(lyrics, key=itemgetter(1), reverse=True)
+    top20_lyrics = sorted_lyrics[1:21]
+    df_lyrics = pd.DataFrame(top20_lyrics, columns=['Lyric', 'Count'])
+    print(df_lyrics)
+
+    # print(word_lyrics_count)
+    return ''
 
 def get_song_details(song_id):
     song_dict = {}
@@ -58,6 +89,7 @@ def results():
         print(request.values)
         if request.method == "POST":
             song_data_dict = get_song_details(request.form["song_id"])
+            build_wordcloud(song_data_dict["lyrics"])
             # print(song_data_dict)
             return render_template("base.html", song_data=song_data_dict)
     except:
